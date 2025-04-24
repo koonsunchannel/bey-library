1c677b59f8778a05b3c48258c0118268b2670571
koonsunchanel3 <koonsunchanel3@gmail.com>
2025-04-24 14:37:22 +0700

    add pic click1

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -25,12 +25,14 @@ export function ProductCard({ id, name, image, category, price }: ProductCardPro
     <Card className="overflow-hidden transition-all duration-200 hover:border-white border-muted cyber-border">
       <CardHeader className="p-0">
         <div className="aspect-square w-full overflow-hidden relative bg-black">
-          <Image
-            src={image}
-            alt={name}
-            fill
-            className="object-contain"
-          />
+          <Link href={`/product/${category}/${id}`}>
+            <Image
+              src={image}
+              alt={name}
+              fill
+              className="object-contain cursor-pointer"
+            />
+          </Link>
         </div>
       </CardHeader>
 
