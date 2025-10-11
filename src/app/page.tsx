7a872b11da4487c402bb3b2bfdf2dddcc2f8754d
SolarTL <solarprisma1@gmail.com>
2025-10-11 13:41:41 +0700

    update patch date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
               <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm cyber-border">Categories</div>
-              <h2 className="text-3xl font-bold tracking-tighter cyber-heading sm:text-5xl">Parts Detail</h2>
+              <h2 className="text-3xl font-bold tracking-tighter cyber-heading sm:text-5xl">Parts Detail Database</h2>
               <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Look for every parts detail in any sections.
               </p>
